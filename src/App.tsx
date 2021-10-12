b55073fb1e06388d9574e4bb6e6d13c104c82494
FaziletKosure <faziletkosure1@gmail.com>
2021-10-12 19:10:26 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { fetchQuizQuestions } from './API';
 
-function App() {
+// components
+
+import QuestionCard from "./components/QuestionCard"
+
+// Types 
+
+import { Difficulty } from './API';
+
+const TOTAL_QUESTIONS=10;
+
+const App=()=> {
+
+  const [loadind, setLoading]=useState(false);
+  const [questions, setQuestions]=useState([]);
+  const [number, setNumber]=useState(0);
+  const [userAnswers, setUserAnswers]=useState([]);
+  const [score, setScore]=useState(0);
+  const [gameOver, setGameOver]=useState(true);
+
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS,Difficulty.EASY));
+  
+
+
+  const startTrivia=async()=>{
+
+  }
+
+  const checkAnswer=(e: React.MouseEvent<HTMLButtonElement>)=>{
+
+  }
+
+  const nextQuestion=()=>{
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>REACT QUIZ</h1>
+     <button className="start" onClick={startTrivia}>
+       Start
+     </button>
+     <p className="score">Score:</p>
+     <p>Loading Questions ...</p>
+     {/* <QuestionCard
+     questionNr={number +1}
+     totalQuestions={TOTAL_QUESTIONS}
+     question={questions[number].question}
+     answers={questions[number].answers}
+     userAnswer={userAnswers ? userAnswers[number]: undefined}
+     callback={checkAnswer}
+
+     /> */}
+     <button className='next' onClick={nextQuestion}>
+       Next Question
+     </button>
     </div>
   );
 }
